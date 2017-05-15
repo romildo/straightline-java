fc55fa26c73231b2b6c35d2a78a34df7be4f5cba
romildo <malaquias@gmail.com>
2017-05-14 22:58:37 -0300

    Fix minor errors

---
+++ src/main/java/CompoundStm.java
@@ -0,0 +1,18 @@
+public class CompoundStm extends Stm {
+
+   public final Stm stm1;
+   public final Stm stm2;
+
+   public CompoundStm(Stm stm1, Stm stm2) {
+      this.stm1 = stm1;
+      this.stm2 = stm2;
+   }
+
+   @Override
+   public String toString() {
+      return "CompoundStm{" +
+             "stm1=" + stm1 +
+             ", stm2=" + stm2 +
+             '}';
+   }
+}
