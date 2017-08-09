af8506d3a5efa0a4583907575a6098fc080a4e92
romildo <malaquias@gmail.com>
2017-08-09 09:19:47 -0300

    Add EOF token

--- src/main/java/lexer/Token.java
+++ src/main/java/lexer/Token.java
@@ -2,16 +2,12 @@ package lexer;
 
 public class Token {
    public enum T {
-      COMMA,
-      PRINT,
-      ID,
-      INT,
+      EOF, COMMA, PRINT, ID, INT,
       // ... definir os outros tipos de símbolos léxicos
-      EOF
    }
 
-   public T type;
-   public Object val;
+   public final T type;
+   public final Object val;
 
    public Token(T type, Object val) {
       this.type = type;
